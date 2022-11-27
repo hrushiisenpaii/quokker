ce340ce1fdd26790a96d51c2ada7d72be0c439b9
Hrushikesh Patra <hrushikesh.patra24@gmail.com>
2022-11-27 21:53:04 +0530

    quokker

---
+++ README.md
@@ -0,0 +1,1 @@
+# quokker
---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'eabaf5e84cmsh7e077c936e3ef93p1f020fjsnad03ba6fe338',
+		'X-RapidAPI-Host': 'famous-quotes4.p.rapidapi.com'
+	}
+};
+
+function getResults () {
+  fetch('https://famous-quotes4.p.rapidapi.com/random?category=all&count=1', options)
+	.then(response => {
+    return response.json();
+  })
+  .then(displayResults)
+}
+
+function displayResults (response) {
+  let quote = document.querySelector('.container .quote');
+  quote.innerText = response[0].text;
+
+  let auth = document.querySelector('.container .author');
+  auth.innerText = "- " + response[0].author;
+}
+
+
+
+document.addEventListener("DOMContentLoaded",
+  function (event){
+
+    getResults();
+    
+  
+  })
+
+
+
